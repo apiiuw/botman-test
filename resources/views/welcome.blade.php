35ec00058d9b512fa1b59e19dc30a19a94c83550
Rafi Rizqallah Andila <2210501029@mahasiswa.upnvj.ac.id>
2024-06-02 00:31:22 +0700

    update awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Nirmabot Test</title>
+        <title>P3M Botchat Test</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body style="background-color: rgb(66, 66, 66)">
@@ -12,14 +12,22 @@
    
     <script>
         var botmanWidget = {
-            title: 'LAPORKAN MASALAH',
+            title: 'P3M Bem UPNVJ',
+            titleTextColor: '#FFFFFF',
             introMessage: "Selamat datang di Nirmabot! beritahu kami masalahmu?",
             placeholderText: 'Kirimkan pesan...',
-            bubbleAvatarUrl: '{{ asset('LOGO N.png') }}',
-            aboutText: 'BEM-U UPNVJ',
-            
+            bubbleBackground: '#EDDFDF',
+            mainColor: '#D46882',
+            aboutText: 'P3M UPNVJ',
+            aboutLink: 'https://bemupnvj.com',
         };
     </script>
+
+    <style>
+        .element-style {
+            color:white;
+        }
+    </style>
    
     <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
        
